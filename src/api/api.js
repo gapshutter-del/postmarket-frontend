d9b869160d6a8ab0ad6aa136f477b4f1e7897841
Rapelang Ramoupi <ramoupi33@gmail.com>
2026-07-27 15:30:11 +0200

    Use environment variable for API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,3 @@
-import axios from "axios";
-
 const api = axios.create({
-  baseURL: "https://postmarket-v2.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default api;
